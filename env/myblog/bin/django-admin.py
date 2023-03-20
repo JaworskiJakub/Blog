9ed0c942b32cf471877d883cf4c6317fba0ba59b
Jakub Jaworski <115419319+JaworskiJakub@users.noreply.github.com>
2023-03-20 21:27:04 +0100

    New project creation

---
+++ env/myblog/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jakubjaworski/Desktop/Projekty Django/Blog/env/myblog/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
