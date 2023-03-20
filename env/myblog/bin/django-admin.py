37bb7817de5506265fbfe80f2fd833efb24bd4d7
Jakub Jaworski <115419319+JaworskiJakub@users.noreply.github.com>
2023-03-20 22:22:56 +0100

    venv and administration site

--- env/myblog/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/jakubjaworski/Desktop/Projekty Django/Blog/env/myblog/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
